4249000fff4a65373bbef665c193e76ed6ebe869
a10gymbackupjotape-lang <jotapelessa@MacBook-Air-de-jotapelessa.local>
2026-09-04 11:17:59 -0300

    chore: update APK print version to v062 in compile script

--- compile_apk.sh
+++ compile_apk.sh
@@ -18,4 +18,4 @@ if [ -f "app/build/outputs/apk/release/app-release.apk" ]; then
     cp app/build/outputs/apk/release/app-release.apk ../sentinela-android-latest.apk
 fi
 
-echo "APKs v061 gerados na raiz do projeto."
+echo "APKs v062 gerados na raiz do projeto."
